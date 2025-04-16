66ce6127a70032885fc181ed1fbd771ec1eef286
LuciaCarballo <lulucarcas98@gmail.com>
2025-04-16 21:08:00 +0200

    casi ejercicio

---
+++ 5.Funciones I/01_Multiplicación/js/main.js
@@ -0,0 +1,31 @@
+function Multiplicacion () {
+  let valor1 = 6;
+  let valor2 = 8;
+  let multiplicacion = valor1*valor2;
+  console.log(multiplicacion)
+  
+}
+
+Multiplicacion ();
+
+
+function Media (valor1, valor2, valor3, valor4, valor5) {
+    let media = (valor1+valor2+valor3+valor4,valor5)/5 
+    console.log(media)
+}
+
+Media (2,3,4,5,6)
+
+Media (5,7,8,5,7)
+
+Media (5,7,9,5,9)
+
+Media (5,7,9,5,6)
+
+
+
+function Pares () {
+
+
+
+}
